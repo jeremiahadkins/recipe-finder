103b226b45ef02c555cd60fb6241950cb4be8664
Jeremiah Adkins <hi@jeremiahadkins.com>
2017-08-09 23:39:25 -0400

    recipe finder app

---
+++ app/scripts/index.js
@@ -0,0 +1,47 @@
+let $ = require('jquery');
+let handlebars = require('handlebars');
+
+let $searchBox = $('.recipe-search'); 
+let url = 'https://recipepuppyproxy.herokuapp.com/api/';
+
+// default data with blank search query
+// fetch(url + '?q=').then(function(response) {
+//   return response.json();
+// }).then(start);
+
+$('.search-btn').on('click', function(event) {
+  event.preventDefault();
+  let userSearch = $searchBox.val();
+  let searchQuery = '?q=' + userSearch;
+
+  $('.recipes-wrapper').empty();
+  fetch(url + searchQuery).then(function(response) {
+    return response.json();
+  }).then(start);
+  console.log(url);
+  console.log(searchQuery);
+});
+
+
+
+function start(ajaxRequest) {
+  let recipes = '';
+  recipes = ajaxRequest.results;
+  displayRecipe(recipes);
+}
+
+
+function displayRecipe(recipes){
+  // this is the script tag dumbass
+  // source is the template
+  let $source = $('.recipe-template').html();
+  
+  // this compiles the script in previous tag
+  // need to compile source(template)
+  let template = handlebars.compile($source);
+
+  recipes.forEach( function(recipe) {
+    console.log(recipe);
+    $('.recipes-wrapper').append(template(recipe));
+  });
+}
